35583fa89b234ca914a7f6f7539af5efd880ea9e
Gabriel Barbosa <gabryel122crf@gmail.com>
2023-02-18 16:07:12 -0300

    tirando os consoles

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -58,11 +58,7 @@ const Card = ({ onclick }: Props) => {
     alterName(namePokemon, modal.index);
     setShowEdit(!showEdit);
     setEditedPokemon(true);
-    console.log("editedPokemon", editedPokemon);
-    console.log("namePokemon", namePokemon);
-    console.log("dadosPokemon", dadosPokemon)
     const team = getLocalStorage();
-    console.log("teamwwwwwwwwwww", team)
     dispatch({
       type: "SET_POKEMON_EDIT",
       payload: {
